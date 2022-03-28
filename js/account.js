3307482c8f376d69bbe4f31ae0ebd5e0ca4e49e7
ochuko1996 <georgeochuko7@gmail.com>
2022-03-28 22:50:21 +0100

    done with Exchange

---
+++ js/account.js
@@ -0,0 +1,10 @@
+export const account = [
+    {
+        "name":     "ochuko samuel george",
+        "accountNo": "0055944442"        
+    },
+    {
+        "name":     "gbemiga moses",
+        "accountNo": "3843838282"       
+    }
+]
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import {account} from "./account.js"
 const harmburger = document.querySelectorAll('.nav-toggle')
 const navLinks  = document.querySelector('.nav-links')
 const signupBtn  = document.querySelector('.sign-up-drop-down')
@@ -11,3 +12,58 @@ harmburger.forEach(btn=>{
         }
     })
 })
+// select and progress bar js
+const progressBar = document.querySelector('.progress-bar-inner')
+const select = document.querySelectorAll('.select')
+const coin = document.getElementById('coin')
+const bank = document.getElementById('bank')
+const accountNumber = document.getElementById('account-number').value
+let progressBarWidth = 10
+coin.addEventListener("click",()=>{
+    if(coin.selectedIndex > 0){
+        
+        progressBar.style.width = progressBarWidth = progressBarWidth + 30 + "%"
+        progressBar.style.transition = "1s"
+        console.log(progressBarWidth);
+        
+    }
+    else{
+        progressBar.style.width = ""
+    }
+})
+
+bank.addEventListener("click",()=>{
+    if(bank.selectedIndex > 0){
+        progressBar.style.width = progressBarWidth = progressBarWidth + 30 + "%"
+        progressBar.style.transition = "1s"
+    }
+    else{
+        progressBar.style.width = ""
+    }
+})
+// const accountNo = account
+// if(accountNo === accountNumber){
+//     progressBar.style.width = "90%"
+//     progressBar.style.transition = "1s"
+// }
+// else{
+//     progressBar.style.width = ""
+// }
+// console.log(accountNo);
+//Eventlister
+// select.forEach(item=>{
+//     item.addEventListener("click",()=>{
+//         if(item.selectedIndex > 0){
+//             progressBar.style.width = "30%"
+//             progressBar.style.transition = "1s"
+//         }else if(item.selectedIndex > 1){
+//             progressBar.style.width = "60%"
+//             progressBar.style.transition = "1s"
+//         }
+//         else{
+//             progressBar.style.width = ""
+//         }
+//         // console.log(e);
+//     })
+
+// 
